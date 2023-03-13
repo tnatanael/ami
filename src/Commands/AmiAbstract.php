f294c4bdb9805d2cb7b7a6d6d96e908be97133da
Thiago Natanael <thiago@solaristelecom.com>
2023-03-13 12:04:52 +0000

    Corrections fire

--- src/Commands/AmiAbstract.php
+++ src/Commands/AmiAbstract.php
@@ -48,6 +48,8 @@ abstract class AmiAbstract extends Command
             $value = is_null($value) ? Arr::get($this->config, $key, null) : $value;
             $options[$key] = $value;
         }
+
+        Log::info($options);
         $client = $this->connector->create($options);
         $client->then([$this, 'client'], [$this, 'writeException']);
         $this->loop->run();
