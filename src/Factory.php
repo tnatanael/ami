15b24abfb9d3f41b1a4caf2fae94c8eb75672daf
Thiago Natanael <thiago@solaristelecom.com>
2023-03-13 12:50:42 +0000

    Corrections fire

--- src/Factory.php
+++ src/Factory.php
@@ -46,12 +46,12 @@ class Factory
         }
         
         $promise = $this->connector->connect($options['host'].':'.$options['port'])->then(function (ConnectionInterface $stream) {
-
+            Log::info('FullFiled');
             return new Client($stream, new Parser());
         }, function($reason) {
             Log::info($reason);
         });
-        
+
         if (!is_null($options['username'])) {
             $promise = $promise->then(function (Client $client) use ($options) {
                 $sender = new ActionSender($client);
